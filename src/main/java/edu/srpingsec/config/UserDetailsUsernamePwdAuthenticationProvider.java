ebf3f1bae7cf8c9fd17596b6bc14630dbbaa429d
Shanb98 <shanbasnayake@hotmail.com>
2023-12-16 22:48:17 +0530

    getGrantedAuthorities

--- src/main/java/edu/srpingsec/config/UserDetailsUsernamePwdAuthenticationProvider.java
+++ src/main/java/edu/srpingsec/config/UserDetailsUsernamePwdAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package edu.srpingsec.config;
 
+import edu.srpingsec.dao.AuthorityEntity;
 import edu.srpingsec.dao.UserEntity;
 import edu.srpingsec.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class UserDetailsUsernamePwdAuthenticationProvider implements AuthenticationProvider {
@@ -35,9 +37,7 @@ public class UserDetailsUsernamePwdAuthenticationProvider implements Authenticat
         List<UserEntity> user = userRepository.findByEmail(userName);
         if(user.size() > 0){
             if(passwordEncoder.matches(password,user.get(0).getPassword())) {
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(user.get(0).getRole()));
-                return new UsernamePasswordAuthenticationToken(userName,password,authorities);
+                return new UsernamePasswordAuthenticationToken(userName,password,getGrantedAuthorities(user.get(0).getAuthorities()));
             } else{
                 throw new BadCredentialsException("Invalid password!");
             }
@@ -45,7 +45,13 @@ public class UserDetailsUsernamePwdAuthenticationProvider implements Authenticat
             throw new BadCredentialsException("No user registered with this details!");
         }
     }
-
+    private List<GrantedAuthority> getGrantedAuthorities(Set<AuthorityEntity> authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (AuthorityEntity authority : authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
+    }
     @Override
     public boolean supports(Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication); //Type of the authentication
